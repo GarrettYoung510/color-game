fa961fa1e70b764ef81c60b120d945e1bdaf2bfc
Garrett Young <GarrettYoung510@Gmail.com>
2019-07-17 00:05:14 -0700

    fixed correct by updating style.backgroundColor to work in mozilla as well also spaces between syntax color array (0, 0, 0)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,32 @@
 alert('connected');
 
 const colors = [
-    "rgb(255,0,0)",
-    "rgb(0,255,0)",
-    "rgb(0,0,255)",
-    "rgb(0,255,255)",
-    "rgb(255,255,0)",
-    "rgb(255,0,255)",
+    "rgb(255, 0, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 0, 255)",
+    "rgb(0, 255, 255)",
+    "rgb(255, 255, 0)",
+    "rgb(255, 0, 255)",
 ]
 
 let squares = document.querySelectorAll(".square");
-let pickedColor = colors[3];
+const pickedColor = colors[3];
 let colorDisplay = document.getElementById("colorDisplay");
 
 colorDisplay.textContent = pickedColor;
-
-for (let i = 0; i < squares.length; i++){
+for (let i = 0; i < squares.length; i++) {
     // add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
 
     // add click listeners to squares
-    squares[i].addEventListener('click', function(){
-        
-        let clickedColor = this.style.background;
+    squares[i].addEventListener('click', function () {
 
-        if(clickedColor === pickedColor){
+        let clickedColor = this.style.backgroundColor;
+        console.log(pickedColor);
+        console.log(clickedColor);
+        if (clickedColor === pickedColor) {
             alert("Correct!");
-        } else{
+        } else {
             alert("WRONG!!!");
         }
     })
